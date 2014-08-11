99aac94435160a3ef75d08bcbfcaf8d21c8fbc75
Itamar Syn-Hershko <itamar@code972.com>
2014-08-11 23:23:38 +0300

    Dashboards

---
+++ OpsBI.Dashboards/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿namespace OpsBI.Dashboards
+{
+    public class Dashboard
+    {
+        public string user { get; set; }
+
+        public string group { get; set; }
+
+        public string title { get; set; }
+
+        public string dashboard { get; set; }
+    }
+}
--- OpsBI.Dashboards/Program.cs
+++ OpsBI.Dashboards/Program.cs
@@ -1,9 +1,65 @@
-﻿namespace OpsBI.Dashboards
+﻿using System.IO;
+using System.Reflection;
+using NElasticsearch;
+using NElasticsearch.Commands;
+using OpsBI.Dashboards;
+
+namespace OpsBI.Dashboards
 {
     class Program
     {
+        static string KibanaIndexName = "kibana-int";
         static void Main(string[] args)
         {
+            bool haveDashboards = false;
+            var elasticsearchClient = new ElasticsearchRestClient("http://localhost:9200");
+            var dashboard1 = elasticsearchClient.Get<Dashboard>("OpsBI.Messages", "dashboard", KibanaIndexName);
+            if (dashboard1 != null && dashboard1.found)
+            {
+                File.WriteAllText(@"z:\OpsBI.Messages.json", dashboard1._source.dashboard);
+                haveDashboards = true;
+            }
+
+            var dashboard2 = elasticsearchClient.Get<Dashboard>("OpsBI.Endpoints", "dashboard", KibanaIndexName);
+            if (dashboard2 != null && dashboard2.found)
+            {
+                File.WriteAllText(@"z:\OpsBI.Endpoints.json", dashboard2._source.dashboard);
+                haveDashboards = true;
+            }
+
+            if (haveDashboards)
+            {
+                return;
+            }
+
+            var db = new Dashboard
+            {
+                title = "OpsBI.Endpoints",
+                group = "guest",
+                user = "guest",
+                dashboard = GetEmbeddedJson(typeof (Program).Assembly, ".OpsBI.Endpoints.json"),
+            };
+            elasticsearchClient.Index(db, db.title, "dashboard", KibanaIndexName);
+
+            db = new Dashboard
+            {
+                title = "OpsBI.Messages",
+                group = "guest",
+                user = "guest",
+                dashboard = GetEmbeddedJson(typeof(Program).Assembly, ".OpsBI.Messages.json"),
+            };
+            elasticsearchClient.Index(db, db.title, "dashboard", KibanaIndexName);
+        }
+
+        static string GetEmbeddedJson(Assembly assembly, string embeddedResourcePath)
+        {
+            if (!embeddedResourcePath.StartsWith(".")) embeddedResourcePath = "." + embeddedResourcePath;
+
+            using (var stream = assembly.GetManifestResourceStream(assembly.GetName().Name + embeddedResourcePath))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
